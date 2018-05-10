d814a24a97b612cfdb9bf57ecf18c6c0f5180fa8
Jibby Ayo-Ani <jibbyani@gmail.com>
2018-05-10 07:34:56 -0700

    Create permutationPalindrome.java

---
+++ permutationPalindrome.java
@@ -0,0 +1,13 @@
+bool isPalindromePermutation(String s){
+  char[] word = s.toCharArray();
+  HashSet unpairedLetters = new HashSet();
+  if (int i = 0; i < word.length; i++){
+    if set.contains(word[i]) { //is odd
+      set.remove(word[i])
+    }
+    else {
+      set.add(word[i])
+    }
+  }
+  return set.size() < 2 
+  }; 
